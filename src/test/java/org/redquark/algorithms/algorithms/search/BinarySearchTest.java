55f1b0c660e12ed54becc88708abe290d2c4f6b6
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-07-27 16:37:31 +0530

    Feature #26 - Implement Search
    Implemented Binary Search Algorithm

--- src/test/java/org/redquark/algorithms/algorithms/search/BinarySearchTest.java
+++ src/test/java/org/redquark/algorithms/algorithms/search/BinarySearchTest.java
@@ -15,7 +15,6 @@ public class BinarySearchTest {
         assertEquals(-1, testBinarySearch.search(null, 4));
         Integer[] data = {4, -3, 2, 1, 7, 5, 6, 9, 8, -2, -4, 12, -3};
         Arrays.sort(data);
-        // -4,-3,-3,-2,1,2,4,5,6,7,8,9,12
         assertEquals(-1, testBinarySearch.search(data, -12));
         assertEquals(6, testBinarySearch.search(data, 4));
         assertEquals(12, testBinarySearch.search(data, 12));
