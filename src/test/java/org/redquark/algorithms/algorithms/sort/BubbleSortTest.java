04daed3a526b054f860723952a905ff4081e55c8
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-07-27 17:42:05 +0530

    Feature #27 - Implement Sort Part I
    Implemented Bubble Sort Algorithm

---
+++ src/test/java/org/redquark/algorithms/algorithms/sort/BubbleSortTest.java
@@ -0,0 +1,21 @@
+package org.redquark.algorithms.algorithms.sort;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class BubbleSortTest {
+
+    private final Sort<Integer> testBubbleSort = new BubbleSort<>();
+
+    @Test
+    public void testSort() {
+        Integer[] data = new Integer[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        Integer[] expected = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertArrayEquals(expected, testBubbleSort.sort(data));
+
+        data = new Integer[]{64, 34, 25, 12, 22, 11, 90};
+        expected = new Integer[]{11, 12, 22, 25, 34, 64, 90};
+        assertArrayEquals(expected, testBubbleSort.sort(data));
+    }
+}
